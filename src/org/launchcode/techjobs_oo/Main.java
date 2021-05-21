4dc7de8b118b5769e3a71affdaeb94770aa0f37f
James Margherio <jimargherio@me.com>
2021-05-21 13:27:59 -0500

    DRY code

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
-        Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
-
-        Job jobX = new Job("Console Cowboy", new Employer("Wintermute"), new Location("Cyberspace"), new PositionType(), new CoreCompetency("Desperation"));
-        Job jobZ = new Job();
-
-        ArrayList<Job> jobs = new ArrayList<>();
-        jobs.add(job1);
-        jobs.add(job2);
-        jobs.add(job3);
-
-        jobs.add(jobX);
-        jobs.add(jobZ);
-
-        for (Job job : jobs){
-            System.out.println(job);
-        }
-    }
+//    public static void main(String[] args) {
+//        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+//        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+//        Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
+//
+//        Job jobX = new Job("Console Cowboy", new Employer("Wintermute"), new Location("Cyberspace"), new PositionType(), new CoreCompetency("Desperation"));
+//        Job jobZ = new Job();
+//
+//        ArrayList<Job> jobs = new ArrayList<>();
+//        jobs.add(job1);
+//        jobs.add(job2);
+//        jobs.add(job3);
+//
+//        jobs.add(jobX);
+//        jobs.add(jobZ);
+//
+//        for (Job job : jobs){
+//            System.out.println(job);
+//        }
+//    }
 
 }
